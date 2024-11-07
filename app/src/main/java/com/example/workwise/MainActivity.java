42c8754969d6ade47a9f015b18d2939180874693
Arielivanitzki <r3dplsunban@gmail.com>
2024-11-07 10:27:02 +0200

    lmk

--- app/src/main/java/com/example/workwise/MainActivity.java
+++ app/src/main/java/com/example/workwise/MainActivity.java
@@ -1,24 +1,52 @@
 package com.example.workwise;
 
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 
 import androidx.activity.EdgeToEdge;
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.core.graphics.Insets;
 import androidx.core.view.ViewCompat;
 import androidx.core.view.WindowInsetsCompat;
+import androidx.fragment.app.Fragment;
 
-public class MainActivity extends AppCompatActivity {
+public class MainActivity extends AppCompatActivity implements View.OnClickListener {
+
+    Button LogBtn;
+    Button SignBtn;
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        EdgeToEdge.enable(this);
         setContentView(R.layout.activity_main);
-        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main), (v, insets) -> {
-            Insets systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars());
-            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom);
-            return insets;
-        });
+
+        LogBtn = findViewById(R.id.LoginBtn);
+        SignBtn = findViewById(R.id.SignUpBtn);
+
+
+        LogBtn.setOnClickListener(this);
+        SignBtn.setOnClickListener(this);
+
+
+    }
+
+    private void loadFragment(Fragment fragment) {
+        getSupportFragmentManager().beginTransaction().replace(R.id.main, fragment).commit();
+    }
+
+    @Override
+    public void onClick(View view) {
+        if(view == LogBtn){
+            loadFragment(new LoginFragment());
+
+        }
+
+        if(view == SignBtn){
+            loadFragment(new SignUpFragment());
+
+        }
+
     }
 }
